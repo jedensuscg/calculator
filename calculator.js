93273a09bb2518f5125ac795a6372d1bd4c4f0fb
James Edens <jedensuscg@gmail.com>
2019-10-22 00:40:07 +0000

    Fixed duplicate outputs of total

--- calculator.js
+++ calculator.js
@@ -1,23 +1,61 @@
 var runningTotal;
 var finalTotal;
+var runCalc = true;
+var test = 0;
 var readline = require('readline-sync');
 var operators = {
-    1: add,
-    2: sub,
-    3: multiply,
-    4: divide,
+    ADD: add,
+    SUBTRACT: sub,
+    MULTIPLY: multiply,
+    DIVIDE: divide,
 }
-initalValue()
 
 
-function initalValue(){
-   num1 = readline.question("enter first number ")
-   oper = readline.question("enter operation: ")
-   num2 = readline.question("enter second value: ")
+initalValue();
 
-   total = operators[oper](num1,num2)
-   console.log("Total: " + total)
 
+
+function initalValue(num1 = null){
+
+    
+    while(runCalc) {
+        !num1 ? num1 = readline.question("Enter the first number: ") : num1
+        oper = readline.question("enter operation: ")
+        num2 = readline.question("enter second value: ")
+        console.log(`num1: ${num1}  num2: ${num2}`)
+        runningTotal =  operators[oper](num1, num2);
+        num1 = runningTotal;
+        console.log("Running Total: " + runningTotal)
+        checkForFinish(runningTotal);
+    }
+
+
+
+
+}
+
+function checkForFinish(runningTotal) {
+
+    var finalTotal = runningTotal;
+    
+
+    finish = readline.question("Continue? y/n? " )
+    if(finish === "y") {
+        return;
+    }
+    if(finish === "n") {
+        equals(finalTotal);
+        runCalc = false;
+
+    }
+
+}
+
+function equals(finalTotal) {
+    test++;
+    console.log(test)
+
+    console.log("Total: " + finalTotal);
 
 }
 
