a7b6a2ba237232104b7b3dd1a850e3a5a2cb49d7
James Edens <jedensuscg@gmail.com>
2019-11-06 15:02:11 +0000

    finised most of calc. First visual pass for CSS

--- calculator.js
+++ calculator.js
@@ -12,6 +12,9 @@ const operatorKeys = document.querySelectorAll('#operator');
 const numberKeys = document.querySelectorAll('.numberKey');
 const decimalKey = document.querySelector('#keyDot');
 const clearKey = document.querySelector('#keyClear');
+const equalKey = document.querySelector('#keyEqual');
+const historySpan = document.querySelector('#historySpan');
+const errorTextField = document.querySelector('#errorText');
 
 
 //#region =====ENUMS AND MODULES====
@@ -21,12 +24,6 @@ var operators = {
     2: sub,
     3: multiply,
     4: divide,
-}
-var operatorText = {
-    1: "Enter the number to ADD: ",
-    2: "Enter the number to SUBTRACT: ",
-    3: "Enter the number to MULTIPLY by: ",
-    4: "Enter the number to DIVIDE by: ",
 }
 var operatorShortText = {
     1: "+",
@@ -40,6 +37,7 @@ var mathHistory = [];
 var display = (function () {
     var upperDisplayText = '';
     var lowerDisplayText = "0";
+    var errorText = '';
     operatorTextDisplay.textContent = '  '
 
     //#region Upper Display Functions
@@ -53,7 +51,7 @@ var display = (function () {
     function setUpperDisplayText(number) {
         let decimals = countDecimalPlaces(number);
         upperDisplayText = parseFloat(number).toFixed(decimals);
-        upperDisplayField.value = upperDisplayText;
+        upperDisplayField.textContent = upperDisplayText;
     }
     //#endregion
 
@@ -74,24 +72,37 @@ var display = (function () {
         }
         else {
             lowerDisplayText = lowerDisplayText + number;
+
         }
-        lowerDisplayField.value = lowerDisplayText;
+        lowerDisplayField.textContent = lowerDisplayText;
+
+    }
+
+    function setErrorText(error) {
+        errorText = error;
+        errorTextField.textContent = errorText;
     }
 
     function updateLowerDisplay() {
         lowerDisplayText = '0';
-        lowerDisplayField.value = lowerDisplayText;
+        lowerDisplayField.textContent = lowerDisplayText;
     }
     //#endregion
 
     function clearDisplays() {
         lowerDisplayText = '';
-        lowerDisplayField.value = lowerDisplayText;
+        lowerDisplayField.textContent = lowerDisplayText;
         upperDisplayText = '';
-        upperDisplayField.value = upperDisplayText;
+        upperDisplayField.textContent = upperDisplayText;
         operatorTextDisplay.textContent = '  '
         console.log(upperDisplayText)
         console.log(lowerDisplayText)
+    }
+
+    function equalDisplay() {
+        upperDisplayField.textContent = '';
+        lowerDisplayField.textContent = value.getRunningTotal();
+        operatorTextDisplay.textContent = ' ';
     }
     return {
         getLowerDisplayTextString,
@@ -103,6 +114,8 @@ var display = (function () {
         updateLowerDisplay,
         clearDisplays,
         setLowerOperatorText,
+        equalDisplay,
+        setErrorText,
     }
 })();
 
@@ -110,7 +123,7 @@ var value = (function () {
     var runningTotal = 0;
     var num1 = 0;
     var num2 = 0;
-    
+
     function resetValues() {
         runningTotal = 0;
         num1 = 0;
@@ -160,8 +173,9 @@ function addKeyPadListeners() {
                 display.setLowerDisplayText(e.target.getAttribute('key'))
                 value.setNum2(display.getLowerDisplayValue());
                 value.setRunningTotal(currentOperation(value.getNum1(), value.getNum2()));
-                display.setUpperDisplayText(value.getRunningTotal())
-                console.log(value.getNum1() + operatorShortText[currentOperationText] + value.getNum2() + " = " + value.getRunningTotal())
+                display.setUpperDisplayText(value.getRunningTotal());
+
+
             }
         })
     });
@@ -169,7 +183,7 @@ function addKeyPadListeners() {
     decimalKey.addEventListener('click', (e) => {
         decimalTrue = display.getLowerDisplayTextString().includes('.');
         console.log(decimalTrue)
-        if(!decimalTrue) {
+        if (!decimalTrue) {
             display.setLowerDisplayText('.');
         }
     });
@@ -184,21 +198,26 @@ function addOperatorListeners() {
             if (display.getUpperDisplayTextString() == '') {
                 var number = display.getLowerDisplayTextString();
                 display.setUpperDisplayText(number);
+                currentOperation = operators[e.target.getAttribute('data')];
+                currentOperationText = e.target.getAttribute('data')
+                mathHistory.push(display.getLowerDisplayTextString(),' ', operatorShortText[currentOperationText],' ');
                 display.updateLowerDisplay();
                 value.setNum1(number);
                 currentOperation = operators[e.target.getAttribute('data')];
                 currentOperationText = e.target.getAttribute('data')
-                display.setLowerOperatorText(currentOperationText);
+                display.setLowerOperatorText(currentOperationText); 
+                historySpan.textContent = updateHistory();
             }
             else {
-                let number = display.getLowerDisplayTextString();
                 display.setUpperDisplayText(value.getRunningTotal());
-                display.updateLowerDisplay(currentOperationText);
-                value.setNum1(value.getRunningTotal());
                 currentOperation = operators[e.target.getAttribute('data')];
                 currentOperationText = e.target.getAttribute('data')
-                console.log(currentOperationText)
+                mathHistory.push(display.getLowerDisplayTextString(),' ', operatorShortText[currentOperationText],' ');
+                display.updateLowerDisplay(currentOperationText);
+                value.setNum1(value.getRunningTotal());
                 display.setLowerOperatorText(currentOperationText);
+
+                historySpan.textContent = updateHistory();
             }
         });
     });
@@ -208,6 +227,12 @@ function addOperatorListeners() {
         number = ''
         currentOperation = undefined;
         console.log(value.getNum1())
+    });
+    equalKey.addEventListener('click', (e) => {
+        mathHistory.push(display.getLowerDisplayTextString(), ' = ', value.getRunningTotal());
+        historySpan.textContent = updateHistory(true);
+        display.equalDisplay();
+
     });
 }
 
@@ -228,147 +253,20 @@ function countDecimalPlaces(number) {
 
 }
 
-/* 
-function runCalculator(number, operation) {
+function updateHistory(finished = false) {
+    let firstComma = mathHistory.toString().lastIndexOf(',');
+    console.log("first comma" + firstComma)
+    let historyFull = mathHistory.join('')
+    let historyPartial = mathHistory.slice();
+   if(!finished) {
+    historyPartial.pop();
+    historyPartial.pop();
+    historyPartial.push(' = ');
+   }
 
-    mathHistory.push(number);
-    mathHistory.push(operatorShortText[operation]);
-    value.setNum1(number);
-
-    firstRun = false;
-    let tempTotal = operators[operation](num1, num2);
-    value.setRunningTotal(tempTotal);
-    console.log(`***** YOU DID: ${num1}${operatorShortText[operation]}${num2} = ${value.getRunningTotal()} *****`)
-    let historyTotal = "= " + value.getRunningTotal();
-    mathHistory.push(operatorShortText[operation], num2, historyTotal);
-    num1 = value.getRunningTotal();
-    updateHistory(value.getRunningTotal());
-    askContinueWithValue();
-} */
-
-/* function restartCalc(isRestartable) {
-    if (isRestartable) {
-        restart = readline.question("Restart calculator? y/n: ");
-        if (restart == "y") {
-            firstRun = true;
-            runCalc = true;
-            mathHistory = [];
-            return;
-        }
-        else if (restart == "n") {
-            console.log("Good-Bye")
-            runCalc = false;
-            firstRun = false;
-            return;
-        }
-    }
-}
- */
-//#region =====VALIDATION AND NUMBER ASSIGNMENT=====
-function getFirstNumber() {
-    while (true) {
-        number = parseFloat(readline.question("*** Enter the first number you wish to do math on: "));
-        if (isNaN(number)) {
-            console.log("!!! You entered an invalid number. !!!");
-        }
-
-        else {
-            return number;
-        }
-    }
-}
-function getOperation(number) {
-    while (true) {
-        operation = readline.question(`enter operation: \n 1: ADD \n 2: SUBTRACT \n 3: MULTIPLY \n 4: DIVIDE \n   `)
-        if (operation == 1 || operation == 2 || operation == 3) {
-            return operation;
-        }
-        else if (operation == 4 && number == 0) {
-            console.log("You are trying to divide by zero!!!");
-            while (true) {
-                let choice = readline.question("Would you like to select a different operation?: y/n");
-
-                if (choice == "y") {
-                    break;
-                }
-                else if (choice == "n") {
-                    console.log("TOO BAD, I am not going to puncture a hole in space-time just for your amusement.");
-                    readline.question("Press enter to continue selecting a different operation....outlaw!");
-                    break;
-                }
-                else {
-                    console.log("Invalid Selection!");
-                }
-            }
-        }
-        else if (operation == 4) {
-            return operation;
-        }
-        else {
-            console.log("Please select a valid number/operation");
-        }
-    }
-}
-
-function getSecondNumber(operation) {
-    while (true) {
-        number = parseFloat(readline.question(operatorText[operation]));
-        if (isNaN(number)) {
-            console.log("!!! You entered an invalid number. !!!");
-        }
-        else if (number == 0 && operation == 4) {
-            console.log("!!! You are trying to divide by zero!!!!!! SERIOUSLY? ARE YOU TRYI...I mean, 'Please try again' !!!")
-        }
-        else {
-            return number;
-        }
-    }
-}
-//#endregion
-
-function updateHistory(historyTotal) {
-    historyTotal = "= " + historyTotal;
-    showHistory = readline.question("??? Do you wish to see a history of operations leading to this value? (y/n): ")
-    if (showHistory === "y") {
-        for (var i = 0; i < mathHistory.length; i++) {
-            console.log(mathHistory[i]);
-        }
-        return;
-    }
-    if (showHistory === "n") {
-        console.log("\n")
-        return;
-    }
-
-}
-
-function askContinueWithValue() {
-    /*     if (dividedByZero) {
-            console.log("DIVISION BY ZERO ERROR!!!!!!!! BROKEN")
-            runCalc = false;
-            return;
-        } */
-    continueMath = readline.question(`??? Continue doing math with this value |${value.getRunningTotal()}|? y/n: `)
-    if (continueMath === "y") {
-        console.log(`**** First number is ${value.getRunningTotal()}`)
-        isRestartable = false;
-        return;
-    }
-    if (continueMath === "n") {
-        equals(value.getRunningTotal());
-        isRestartable = true;
-        runCalc = false;
-
-        return;
-    }
-
-
-}
-
-function equals(finalTotal) {
-    console.log(`Your final total for this session is : ||${finalTotal}||`);
-    updateHistory(finalTotal);
-
+    console.log(historyFull)
+    console.log(historyPartial);
+    return historyPartial.join('');
 }
 
 //#region =====FORMULAS=====
